495f770d7cf4ba4b893ba5dc50dea6648ceeef3d
Rossel Thompson <rossel96t@gmail.com>
2022-06-14 11:12:07 -0600

    remove firebase credentials

--- providers/firebase.js
+++ providers/firebase.js
@@ -1,13 +1,13 @@
 import { getApps, initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyD616RFq8TywcWu8e-xTeOvrh06u7VmO1k",
-	authDomain: "react-app-course-85c1f.firebaseapp.com",
-	databaseURL: "https://react-app-course-85c1f.firebaseio.com",
-	projectId: "react-app-course-85c1f",
-	storageBucket: "react-app-course-85c1f.appspot.com",
-	messagingSenderId: "997202904393",
-	appId: "1:997202904393:web:6af76fecd56fbf520ac698",
+	apiKey: "###",
+	authDomain: "###",
+	databaseURL: "###",
+	projectId: "###",
+	storageBucket: "###",
+	messagingSenderId: "###",
+	appId: "###",
 };
 
 const initFirebase = () =>{
